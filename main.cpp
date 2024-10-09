214f36b3fcd6b3ae3cefce7bd0a2ebc570337f66
nad11 <nad11@tpu.ru>
2024-10-09 16:35:23 +0700

    ihateCICD

--- main.cpp
+++ main.cpp
@@ -5,18 +5,6 @@
 #include <algorithm>
 #include <tuple>
 
-// bool comp(const std::vector<std::string> &a, const std::vector<std::string> &b)
-// {
-//     for(int i=0;i<a.size();i++)
-//     {
-//         if (std::stoi(a[i]) != std::stoi(b[i])) 
-//         {
-//             return std::stoi(a[i]) < std::stoi(b[i]);
-//         }
-//     }
-//     return false;
-// }
-
 
 int main()
 {
--- test_version.cpp
+++ test_version.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE test_version
 
 #include "lib.h"
+#include "compare.h"
 
 #include <boost/test/unit_test.hpp>
 
@@ -8,7 +9,10 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() > 0);
+    std::vector<std::vector<std::string>> arr_test{{"192"},{"181"},{"193"},{"46"},{"78"}};
+    BOOST_CHECK(comp(arr_test[1],arr_test[0]));
+    BOOST_CHECK(comp(arr_test[4],arr_test[0]));
+    BOOST_CHECK(comp(arr_test[3],arr_test[4]));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
